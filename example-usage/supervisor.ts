490fd7c4b8f228ac207bace2e55cfd291751f857
giesf <64958447+giesf@users.noreply.github.com>
2025-02-27 14:53:18 +0100

    Update supervisor.ts

--- example-usage/supervisor.ts
+++ example-usage/supervisor.ts
@@ -1,11 +1,35 @@
 
 import { spawn } from "child_process";
-
+import {$} from "bun"
 
 async function main(){
   const ls = spawn('./measure');
-  
+  const hostname = await $`hostname`.text();
   ls.stdout.on('data', (data) => {
+    const data = JSON.parse(data);
+    const entries = Object.entries(data);
+    const timestamp = new Date().valueOf()
+    const jsonL = entries.map([metricName, metricValue]=>{
+      return JSON.stringify({
+        metrics: {
+          __name__: Bun.env.METRIC_PREFIX + metricName,
+          hostname
+        },
+        values: [metricValue],
+        timestamps: [timestamp]
+      })
+    })
+    const server = Bun.env.VM_SERVER
+    const user = Bun.env.VM_USER
+    const password = Bun.env.VM_PASSWORD
+    const res = await fetch(server + "/api/v1/import", {
+            method: "POST",
+            body: jsonL,
+            headers: {
+                Authorization: "Basic " + btoa(user + ":" + password),
+            },
+        })
+    console.log(res.ok)
     console.log(`stdout: ${data}`);
   });
   
